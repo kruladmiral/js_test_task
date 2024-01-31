1826845e2d67651ff3097f994f7084bb98256ec9
Wojciech Sęk <wojtasskate@wp.pl>
2024-01-31 13:10:11 +0100

    v2

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ function check (input) {
         return 'wrong input';
     }
     let array1 = array.splice(3);
-    let array2 = array.splice(input.length + 1);
+    let array2 = array.splice(-array.length);
     
     function getSum (input2) {
         let sum = 0;
@@ -18,4 +18,11 @@ function check (input) {
    return getSum(array1) === getSum(array2) ? 'true' : 'false';
     
 }
-console.log(check(1231234));
+console.log(check(123456));
+console.log(check(7894561123));
+console.log(check('abcdef'));
+console.log(check(456123));
+console.log(check(456456));
+console.log(check([1,2,3,4,5,6]));
+console.log(check({}));
+console.log(check(12345));
